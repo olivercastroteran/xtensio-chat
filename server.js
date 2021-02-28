2537b90cb9e680e761896109f9fe0878fab4a2f5
oliver <olivercastroteran@gmail.com>
2021-02-28 13:29:58 -0400

    socket.io connection stablished & sending messages to the frontend

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
+const { Socket } = require('dgram');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', (socket) => {
+  // Welcome current user
+  socket.emit('message', 'Welcome to Xtensio chat');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
